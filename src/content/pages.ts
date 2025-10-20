0834080f00b88041346e87f415a106f290ddf58a
Rimvydas Bingelis <rimvydas.bin@gmail.com>
2025-10-20 23:50:08 +0300

    added dates reference

--- src/content/pages.ts
+++ src/content/pages.ts
@@ -4,6 +4,7 @@ export const CONTENT_PAGES: ContentMenuItem[] = [
     { menuTitle: '# Basics', contentReference: 'docs/BASE_EXAMPLES.md' },
     { menuTitle: '# Lists', contentReference: 'docs/LIST_FUNCTIONS_REFERENCE.md' },
     { menuTitle: '# Strings', contentReference: 'docs/STRING_FUNCTIONS_REFERENCE.md' },
+    { menuTitle: '# Dates', contentReference: 'docs/DATES_REFERENCE.md' },
     { menuTitle: '# User Types', contentReference: 'docs/USER_TYPES_REFERENCE.md' },
     { menuTitle: '# User Functions', contentReference: 'docs/USER_FUNCTIONS_REFERENCE.md' },
     { menuTitle: 'Playground', type: 'playground' },
